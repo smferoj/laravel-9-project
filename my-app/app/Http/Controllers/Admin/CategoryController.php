6f817175279214cc073b93de2760b1bfe509754f
SM Feroj <sm.feroj@gmail.com>
2023-06-17 07:38:30 +0600

    category-crud-completed

--- my-app/app/Http/Controllers/Admin/CategoryController.php
+++ my-app/app/Http/Controllers/Admin/CategoryController.php
@@ -7,7 +7,8 @@ use Illuminate\Http\Request;
 class CategoryController extends Controller
 {
 public function Index(){
-    return view('admin.allcategory');
+    $categories = Category::latest()->get();
+    return view('admin.allcategory', compact('categories'));
 }
 public function AddCategory(){
     return view('admin.addcategory');
@@ -25,4 +26,26 @@ public function StoreCategory(Request $request){
     return redirect()->route('allcategories')->with('message', 'Category added successfully!');
   }
 
+       public function EditCategory($id){
+        $category_info = Category::findOrFail($id);
+        return view('admin.editcategory', compact('category_info'));
+       }
+
+       public function UpdateCategory(Request $request){
+           $category_id = $request->category_id;
+           $request->validate([
+            'category_name' => 'required|unique:categories|max:255',
+        ]);
+
+        Category::findOrFail($category_id)->update([
+            'category_name' => $request->category_name,
+        'slug'=>strtolower(str_replace(' ','-', $request->category_name))
+        ]);
+        return redirect()->route('allcategories')->with('message', 'Category updated successfully!');
+        }
+
+        public function DeleteCategory($id){
+            Category::findOrFail($id)->delete();
+            return redirect()->route('allcategories')->with('message', 'Category deleted Successfully');
+        }
 }
