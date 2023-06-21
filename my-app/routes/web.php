08387be9659176f2984be8e0746acced6acda74e
SM Feroj <sm.feroj@gmail.com>
2023-06-21 07:36:23 +0600

    backend-complete

--- my-app/routes/web.php
+++ my-app/routes/web.php
@@ -7,12 +7,20 @@ use App\Http\Controllers\Admin\ProductController;
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\SubCategoryController;
+use App\Http\Controllers\HomeController;
 
 
 
-Route::get('/', function () {
-    return view('welcome');
+Route::controller(HomeController::class)->group(function(){
+    Route::get('/', 'Index')->name('Home');
 });
+
+Route::controller(ClientController::class)->group(function(){
+    Route::get('/', 'Index')->name('Home');
+});
+
+
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -49,7 +57,16 @@ Route::controller(SubCategoryController::class)->group(function(){
 Route::controller(ProductController::class)->group(function(){
     Route::get('admin/all-products', 'Index')->name('allproducts');
     Route::get('admin/add-product', 'AddProduct')->name('addproduct');
+    Route::post('/admin/store-product', 'StoreProduct')->name('storeproduct');
+    Route::get('/admin/edit-product-img/{id}', 'EditProductImg')->name('editproductimg');
+    Route::post('/admin/update-product-image', 'UpdateProductImg')->name('updateproductimg');
+    Route::get('/admin/edit-product/{id}', 'EditProduct')->name('editproduct');
+    Route::post('/admin/update-product', 'UpdateProduct')->name('updateproduct');
+    Route::get('/admin/delete-product/{id}', 'DeleteProduct')->name('deleteproduct');
+
 });
+
+
 Route::controller(OrderController::class)->group(function(){
     Route::get('admin/pending-orders', 'Index')->name('pendingorders');
 
